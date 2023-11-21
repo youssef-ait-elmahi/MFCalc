b8cdf7824d45c84321d0ef708f77ec5e3db11a79
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-11-21 03:12:26 +0100

    Refactor calculator buttons and styles

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+// Selecting all the buttons and the result display
+const buttons = document.querySelectorAll('.button');
+const display = document.getElementById('result');
+
+// Adding event listener to the buttons
+buttons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        // Check if clear button is pressed
+        if (e.target.id === 'clear') {
+            display.textContent = '';
+        }
+        // Check if equal button is pressed
+        else if (e.target.id === 'equal') {
+            try {
+                display.textContent = eval(display.textContent);
+            } catch {
+                display.textContent = "Error!";
+            }
+        }
+        // Check if delete button is pressed
+        else if (e.target.id === 'delete') {
+            display.textContent = display.textContent.slice(0, -1);
+        }
+        // Otherwise, add the clicked button's text to the display
+        else {
+            display.textContent += e.target.textContent;
+        }
+    });
+});
