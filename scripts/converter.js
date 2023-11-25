c4c7c72d0d147b619493209b33d23deca7b488a0
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-11-25 20:57:26 +0100

    Add unit conversion functionality for length, weight, speed, time, area, and volume

--- scripts/converter.js
+++ scripts/converter.js
@@ -96,3 +96,120 @@ convert.addEventListener("click", function () {
     output.textContent = "Please enter a valid value and units.";
   }
 });
+
+document.getElementById('convertLength').addEventListener('click', function() {
+  var length = document.getElementById('length').value;
+  var lengthSource = document.getElementById('lengthSource').value;
+  var lengthTarget = document.getElementById('lengthTarget').value;
+
+  var conversionRates = {
+      meters: 1,
+      kilometers: 0.001,
+      miles: 0.000621371,
+      yards: 1.09361,
+      feet: 3.28084,
+      inches: 39.3701
+  };
+
+  var convertedLength = length * conversionRates[lengthSource] / conversionRates[lengthTarget];
+
+  document.getElementById('lengthOutput').value = convertedLength;
+});
+
+// Weight Converter
+document.getElementById('convertWeight').addEventListener('click', function() {
+  var weight = document.getElementById('weight').value;
+  var weightSource = document.getElementById('weightSource').value;
+  var weightTarget = document.getElementById('weightTarget').value;
+
+  var conversionRates = {
+      grams: 1,
+      kilograms: 0.001,
+      ounces: 0.035274,
+      pounds: 0.00220462,
+      stones: 0.000157473
+  };
+
+  var convertedWeight = weight * conversionRates[weightSource] / conversionRates[weightTarget];
+
+  document.getElementById('weightOutput').value = convertedWeight;
+});
+
+// Speed Converter
+document.getElementById('convertSpeed').addEventListener('click', function() {
+  var speed = document.getElementById('speed').value;
+  var speedSource = document.getElementById('speedSource').value;
+  var speedTarget = document.getElementById('speedTarget').value;
+
+  var conversionRates = {
+      kph: 1,
+      mph: 0.621371,
+      mps: 0.277778,
+      fps: 0.911344
+  };
+
+  var convertedSpeed = speed * conversionRates[speedSource] / conversionRates[speedTarget];
+
+  document.getElementById('speedOutput').value = convertedSpeed;
+});
+
+// Time Converter
+document.getElementById('convertTime').addEventListener('click', function() {
+  var time = document.getElementById('time').value;
+  var timeSource = document.getElementById('timeSource').value;
+  var timeTarget = document.getElementById('timeTarget').value;
+
+  var conversionRates = {
+      seconds: 1,
+      minutes: 60,
+      hours: 3600,
+      days: 86400,
+      weeks: 604800,
+      months: 2628000,
+      years: 31536000
+  };
+
+  var convertedTime = time * conversionRates[timeSource] / conversionRates[timeTarget];
+
+  document.getElementById('timeOutput').value = convertedTime;
+});
+
+// Area Converter
+document.getElementById('convertArea').addEventListener('click', function() {
+  var area = document.getElementById('area').value;
+  var areaSource = document.getElementById('areaSource').value;
+  var areaTarget = document.getElementById('areaTarget').value;
+
+  var conversionRates = {
+      sqMeters: 1,
+      sqKilometers: 0.000001,
+      sqMiles: 0.000000386102,
+      acres: 0.000247105,
+      hectares: 0.0001
+  };
+
+  var convertedArea = area * conversionRates[areaSource] / conversionRates[areaTarget];
+
+  document.getElementById('areaOutput').value = convertedArea;
+});
+
+// Volume Converter
+document.getElementById('convertVolume').addEventListener('click', function() {
+  var volume = document.getElementById('volume').value;
+  var volumeSource = document.getElementById('volumeSource').value;
+  var volumeTarget = document.getElementById('volumeTarget').value;
+
+  var conversionRates = {
+      liters: 1,
+      milliliters: 1000,
+      cubicMeters: 0.001,
+      cubicFeet: 0.0353147,
+      gallons: 0.264172,
+      pints: 2.11338
+  };
+
+  var convertedVolume = volume * conversionRates[volumeSource] / conversionRates[volumeTarget];
+
+  document.getElementById('volumeOutput').value = convertedVolume;
+});
+
