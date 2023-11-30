47d3f1ff8bc201904a7ec2ef2ac5f220769e737b
youssef-ait-elmahi <youssefaitelmahi3@gmail.com>
2023-11-30 18:32:56 +0100

    Refactor website layout and styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,13 @@
-$(document).on('mousemove', function(e){
-    $('.cursor').css({
-       left:  e.pageX,
-       top:   e.pageY
+document.addEventListener("DOMContentLoaded", function () {
+    // Select the "About" section
+    const aboutSection = document.querySelector('.about');
+
+    // Add click event listener to the "About" link
+    document.getElementById('about-link').addEventListener('click', function (event) {
+        // Prevent the default link behavior
+        event.preventDefault();
+
+        // Toggle the visibility of the "About" section
+        aboutSection.style.display = aboutSection.style.display === 'none' ? 'block' : 'none';
     });
 });
